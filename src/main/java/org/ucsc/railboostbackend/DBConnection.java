adb1230cf699071c46da546227fabfe2948c41d3
DewBM <dewmitha.mahiti@gmail.com>
2023-09-19 19:52:33 +0530

    implement the database connection class

---
+++ src/main/java/org/ucsc/railboostbackend/DBConnection.java
@@ -0,0 +1,22 @@
+package org.ucsc.railboostbackend;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private final String host = "localhost";
+    private final String port = "3306";
+    private final String db_name = "RailBoost";
+    private final String username = "root";
+    private final String password = "";
+
+    public Connection getInstance() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        String dbUrl = "jdbc:mysql://"+host+":"+port+"/"+db_name;
+
+        System.out.println(dbUrl);
+
+        return DriverManager.getConnection(dbUrl, username, password);
+    }
+}
