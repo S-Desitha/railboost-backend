b55a5679f759c450591baf3a4a2a79542108f443
DewBM <dewmitha.mahiti@gmail.com>
2023-09-25 16:46:16 +0530

    update LoginController class to use password hashing.

--- src/main/java/org/ucsc/railboostbackend/Controllers/LoginController.java
+++ src/main/java/org/ucsc/railboostbackend/Controllers/LoginController.java
@@ -1,16 +1,16 @@
 package org.ucsc.railboostbackend.Controllers;
 
 import org.ucsc.railboostbackend.Utilities.DBConnection;
+import org.ucsc.railboostbackend.Utilities.HashPassword;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.security.spec.InvalidKeySpecException;
+import java.sql.*;
+import java.util.Map;
 
 public class LoginController extends HttpServlet {
     @Override
@@ -28,7 +28,22 @@ public class LoginController extends HttpServlet {
         String username = req.getParameter("username");
         String password = req.getParameter("password");
 
-        String query = String.format("SELECT username, password FROM users WHERE username=\"%s\" AND password=\"%s\"", username, password);
+        if (validateUser(username, password))
+            writer.write("Login Successful");
+        else
+            writer.write("Invalid username or password.\nPlease try again!!");
+
+        writer.flush();
+        writer.close();
+
+//        temp(username, password);
+    }
+
+
+    private boolean validateUser(String username, String inputPassword) {
+        HashPassword hashPassword = new HashPassword();
+
+        String query = String.format("SELECT password, salt FROM users WHERE username=\"%s\"", username);
         Statement statement = null;
         ResultSet result = null;
         try {
@@ -37,11 +52,15 @@ public class LoginController extends HttpServlet {
             statement = connection.createStatement();
             result = statement.executeQuery(query);
 
-            resp.setContentType("text/plain");
-            if (result.next())
-                writer.write("Login Successful");
-            else
-                writer.write("Invalid username or password. Try again!");
+            while (result.next()) {
+                String hash = result.getString("password");
+                String salt = result.getString("salt");
+
+                // entered password matches with the password stored with the given username.
+                if (hashPassword.hash(inputPassword, salt).equals(hash)) {
+                    return true;
+                }
+            }
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
@@ -57,5 +76,42 @@ public class LoginController extends HttpServlet {
                 System.out.println(e.getMessage());
             }
         }
+
+
+        return false;
+    }
+
+
+    private void temp(String username, String password) {
+
+        try {
+            Map<String, String> hashResult = new HashPassword().hash(password);
+
+            String hash = hashResult.get("hash");
+            String salt = hashResult.get("salt");
+
+            System.out.println("Hashed Password: "+ hash);
+            System.out.println("Salt: " + salt);
+
+            String sql = "INSERT INTO users VALUES(?, ?, ?)";
+            PreparedStatement statement = null;
+            ResultSet result = null;
+            try {
+                Connection connection = DBConnection.getInstance().getConnection();
+                statement = connection.prepareStatement(sql);
+                statement.setString(1, username);
+                statement.setString(2, hash);
+                statement.setString(3, salt);
+
+                statement.executeUpdate();
+
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+
+        } catch (InvalidKeySpecException e) {
+            System.out.println("Error in hash function.\n"+e.getMessage());
+        }
+
     }
 }
