4ad543951e1f76180b894112168a99218c9c8444
DewBM <dewmitha.mahiti@gmail.com>
2023-12-11 09:17:01 +0530

    add the method to handle get requests.

--- src/main/java/org/ucsc/railboostbackend/controllers/JourneyController.java
+++ src/main/java/org/ucsc/railboostbackend/controllers/JourneyController.java
@@ -1,8 +1,13 @@
 package org.ucsc.railboostbackend.controllers;
 
 import com.google.gson.*;
+import org.ucsc.railboostbackend.enums.Day;
+import org.ucsc.railboostbackend.models.Journey;
 import org.ucsc.railboostbackend.models.JourneyStation;
+import org.ucsc.railboostbackend.models.Schedule;
 import org.ucsc.railboostbackend.repositories.JourneyRepo;
+import org.ucsc.railboostbackend.repositories.ScheduleRepo;
+import org.ucsc.railboostbackend.services.LocalDateDeserializer;
 import org.ucsc.railboostbackend.services.LocalTimeDeserializer;
 import org.ucsc.railboostbackend.utilities.Security;
 
@@ -14,8 +19,11 @@ import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.lang.reflect.Type;
+import java.net.URLDecoder;
 import java.time.*;
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.List;
 
 public class JourneyController extends HttpServlet {
     @Override
@@ -41,6 +49,29 @@ public class JourneyController extends HttpServlet {
             writer.write("You don't have permission for this operation");
         }
 
+        writer.flush();
+        writer.close();
+    }
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        JourneyRepo journeyRepo = new JourneyRepo();
+        Journey journey;
+
+        Gson gson = new GsonBuilder()
+                .registerTypeAdapter(LocalDate.class, LocalDateDeserializer.INSTANCE)
+                .setDateFormat("yyyy-MM-dd")
+                .create();
+
+        String jsonQuery = URLDecoder.decode(req.getParameter("json"), "UTF-8");
+        Journey reqJourney = gson.fromJson(jsonQuery, Journey.class);
+        journey = journeyRepo.getJourney(reqJourney);
+
+//        journey.forEach(System.out::println);
+
+        writer.write(gson.toJson(journey));
         writer.flush();
         writer.close();
     }
