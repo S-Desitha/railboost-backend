2b4fd50328a26a48c6971a14fbbbc015f4aca3af
DewBM <dewmitha.mahiti@gmail.com>
2024-01-10 21:06:23 +0530

    rename file.

--- src/main/java/org/ucsc/railboostbackend/controllers/UserCredentialsController.java
+++ src/main/java/org/ucsc/railboostbackend/controllers/UserCredentialsController.java
@@ -1,13 +1,13 @@
 package org.ucsc.railboostbackend.controllers;
 
 import com.google.gson.Gson;
-import org.ucsc.railboostbackend.models.Auth;
+import io.jsonwebtoken.Claims;
+import org.ucsc.railboostbackend.models.User;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -15,17 +15,14 @@ public class AuthController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter writer = resp.getWriter();
-        HttpSession session = req.getSession();
+        Claims jwt = (Claims) req.getAttribute("jwt");
+        User user = new User();
         Gson gson = new Gson();
 
-        Auth auth = new Auth();
-        if (!session.isNew()) {
-            auth.setUsername((String) session.getAttribute("username"));
-            auth.setRole((String) session.getAttribute("role"));
-        }
+        user.setUsername(jwt.get("username", String.class));
+        user.setRole(jwt.get("role", String.class));
 
-        String json = gson.toJson(auth);
-        writer.write(json);
+        writer.write(gson.toJson(user));
         writer.flush();
         writer.close();
     }
