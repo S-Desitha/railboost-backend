ea80092ae76465d80749f384c0e3694fd9c2e3bf
pehan <pehamrathnayake159@gmail.com>
2024-01-29 20:29:11 +0530

    impelent function to add line

---
+++ src/main/java/org/ucsc/railboostbackend/repositories/LineRepo.java
@@ -0,0 +1,76 @@
+package org.ucsc.railboostbackend.repositories;
+import org.ucsc.railboostbackend.models.Line;
+import org.ucsc.railboostbackend.utilities.DBConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class LineRepo {
+
+    public void addLines(Line line)throws SQLException, ClassNotFoundException{
+        Connection connection = DBConnection.getConnection();
+        String query = "INSERT INTO line(line_id, line_name) VALUES (?,?)";
+
+        try(PreparedStatement statement = connection.prepareStatement(query)){
+            statement.setString(1,line.getLineId());
+            statement.setString(2, line.getLineName());
+
+            statement.executeUpdate();
+        }catch (SQLException e){
+            System.out.println("Error when inserting new entry in line table: "+e.getMessage());
+
+        }
+
+
+    }
+
+    public Line getLineById(String lineId) throws SQLException {
+        Connection connection = DBConnection.getConnection();
+        Line line = new Line();
+        String query = "SELECT line_id, line_name FROM line  WHERE line_id=?";
+        PreparedStatement pst = null;
+        ResultSet resultSet = null;
+
+        //pst = connection.prepareStatement(query);
+
+
+        try(PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setString(1, lineId);
+            resultSet = statement.executeQuery();
+
+            if (resultSet.next())
+                line.setLineId(resultSet.getString("line_id"));
+                line.setLineName((resultSet.getString("line_name")));
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return line;
+        }
+
+        public void updateLine(Line line) throws SQLException {
+        Connection connection = DBConnection.getConnection();
+        String query ="UPDATE `line` SET `line_name`=? WHERE line_id=?;";
+        PreparedStatement statement =connection.prepareStatement(query);
+        statement.setString(1,line.getLineName());
+        statement.setString(2,line.getLineId());
+        statement.executeUpdate();
+
+        }
+
+        public void deleteLine(Line line){
+        Connection connection = DBConnection.getConnection();
+        String query = "DELETE FROM `line` WHERE line_id=?";
+
+        try(PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setString(1,line.getLineId());
+            statement.executeUpdate();
+        } catch (SQLException e){
+            System.out.println("Error occurred while executing delete query for train table");
+        }
+        }
+    }
+
