e754ee3214d01f388685ceeefd79e7a98c273127
Kalindu Sankalpa <kalindusankalpa5285@gmail.com>
2024-02-11 20:11:28 +0530

    change query and db of getting ticketprice

--- src/main/java/org/ucsc/railboostbackend/controllers/RoleController.java
+++ src/main/java/org/ucsc/railboostbackend/controllers/RoleController.java
@@ -1,8 +1,10 @@
 package org.ucsc.railboostbackend.controllers;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.ucsc.railboostbackend.models.Role;
 import org.ucsc.railboostbackend.repositories.RoleRepo;
+import org.ucsc.railboostbackend.services.LocalDateSerializer;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -10,6 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.time.LocalDate;
 import java.util.List;
 
 public class RoleController extends HttpServlet {
@@ -17,7 +20,10 @@ public class RoleController extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter writer = resp.getWriter();
         RoleRepo roleRepo = new RoleRepo();
-        Gson gson = new Gson();
+        Gson gson = new GsonBuilder()
+                .registerTypeAdapter(LocalDate.class, LocalDateSerializer.INSTANCE)
+                .setDateFormat("MM/dd/yyyy")
+                .create();
         List<Role> roles;
 
         roles = roleRepo.getAllRoles();
