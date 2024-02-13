546d0a09ff18d0c14fd47a6f69cf0143dbd811e0
M.M.D.B.MAHITI <136871701+DewBM@users.noreply.github.com>
2024-02-13 15:59:58 +0530

    Update RoleController.java

--- src/main/java/org/ucsc/railboostbackend/controllers/RoleController.java
+++ src/main/java/org/ucsc/railboostbackend/controllers/RoleController.java
@@ -1,10 +1,8 @@
 package org.ucsc.railboostbackend.controllers;
 
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import org.ucsc.railboostbackend.models.Role;
 import org.ucsc.railboostbackend.repositories.RoleRepo;
-import org.ucsc.railboostbackend.services.LocalDateSerializer;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -12,7 +10,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.time.LocalDate;
 import java.util.List;
 
 public class RoleController extends HttpServlet {
@@ -20,10 +17,7 @@ public class RoleController extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter writer = resp.getWriter();
         RoleRepo roleRepo = new RoleRepo();
-        Gson gson = new GsonBuilder()
-                .registerTypeAdapter(LocalDate.class, LocalDateSerializer.INSTANCE)
-                .setDateFormat("MM/dd/yyyy")
-                .create();
+        Gson gson = new Gson();
         List<Role> roles;
 
         roles = roleRepo.getAllRoles();
