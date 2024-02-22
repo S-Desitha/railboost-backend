bdb1fa207557caaf9f6912bc14699e1e17b5f2c5
pehan <pehamrathnayake159@gmail.com>
2024-02-22 15:54:01 +0530

    impelent Email service

--- src/main/java/org/ucsc/railboostbackend/controllers/ParcelBookingController.java
+++ src/main/java/org/ucsc/railboostbackend/controllers/ParcelBookingController.java
@@ -1,2 +1,35 @@
-package org.ucsc.railboostbackend.controllers;public class ParcelBookingController {
+package org.ucsc.railboostbackend.controllers;
+
+import com.google.gson.Gson;
+import io.jsonwebtoken.Claims;
+import org.ucsc.railboostbackend.models.ParcelBooking;
+import org.ucsc.railboostbackend.repositories.ParcelBookingRepo;
+import org.ucsc.railboostbackend.services.MyCache;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Date;
+
+public class ParcelBookingController extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)throws IOException, ServletException {
+        Claims jwt = (Claims) req.getAttribute("jwt");
+        Gson gson = new Gson();
+        ParcelBookingRepo parcelBookingRepo = new ParcelBookingRepo();
+
+        ParcelBooking parcelBooking = gson.fromJson(req.getReader(), ParcelBooking.class);
+        parcelBooking.setUserId(jwt.get("userId", Integer.class));
+        parcelBooking.setTrackingId(new MyCache().createTempUID(new Date().toString()));
+
+        try {
+            parcelBookingRepo.addParcel(parcelBooking);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
--- src/main/java/org/ucsc/railboostbackend/repositories/ParcelBookingRepo.java
+++ src/main/java/org/ucsc/railboostbackend/repositories/ParcelBookingRepo.java
@@ -1,2 +1,44 @@
-package org.ucsc.railboostbackend.repositories;public class ParcelBookingRepo {
+package org.ucsc.railboostbackend.repositories;
+
+import org.ucsc.railboostbackend.models.ParcelBooking;
+import org.ucsc.railboostbackend.services.EmailService;
+import org.ucsc.railboostbackend.utilities.DBConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class ParcelBookingRepo {
+    public void addParcel(ParcelBooking parcelBooking) throws SQLException {
+        Connection connection = DBConnection.getConnection();
+        String parcel_query ="INSERT INTO `parcelbooking`( `trackingId`, `sendingStation`, " +
+                "`senderAddress`, `SenderNIC`, `recoveringStation`, `receiverName`, `receiverAddress`, `receiverTelNo`," +
+                " `receiverEmail`, `item`, `receiverNIC`, `userId`) VALUES (?,?,?,?,?,?,?,?,?,?,?,?);";
+
+        try (PreparedStatement statement = connection.prepareStatement(parcel_query)){
+            statement.setString(1,parcelBooking.getTrackingId());
+            statement.setString(2,parcelBooking.getSendingStation());
+            statement.setString(3,parcelBooking.getReceiverAddress());
+            statement.setString(4,parcelBooking.getSenderNIC());
+            statement.setString(5,parcelBooking.getRecoveringStation());
+            statement.setString(6,parcelBooking.getReceiverName());
+            statement.setString(7,parcelBooking.getSenderAddress());
+            statement.setString(8,parcelBooking.getReceiverTelNo());
+            statement.setString(9,parcelBooking.getReceiverEmail());
+            statement.setString(10,parcelBooking.getItem());
+            statement.setString(11,parcelBooking.getReceiverNIC());
+            statement.setInt(12,parcelBooking.getUserId());
+
+            statement.executeUpdate();
+
+            EmailService emailService =  new EmailService();
+            String body = emailService.createParcelBookingEmail(parcelBooking);
+            emailService.sendEmail(parcelBooking.getReceiverEmail(),"parcel Booking Email",body);
+
+
+
+        }catch (SQLException e){
+            System.out.println("Error when inserting new entry in line table: "+e.getMessage());
+        }
+    }
 }
