6cac7e050bda39da673c2b2ac2382bcf099956a7
Sathira Gardihewa <sathiradesitha@gmail.com>
2024-04-19 02:20:59 +0530

    line management fully done

--- src/main/java/org/ucsc/railboostbackend/controllers/LineController.java
+++ src/main/java/org/ucsc/railboostbackend/controllers/LineController.java
@@ -1,6 +1,7 @@
 package org.ucsc.railboostbackend.controllers;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
 import org.ucsc.railboostbackend.models.Line;
 import org.ucsc.railboostbackend.repositories.LineRepo;
 
@@ -21,14 +22,10 @@ public class LineController extends HttpServlet {
         Gson gson = new Gson();
         LineRepo lineRepo =  new LineRepo();
         List<Line> lineList;
-        Line line;
-        System.out.println("test 0");
-        String lineId = req.getParameter("lineId");
-        System.out.println(lineId);
 
-            System.out.println("test 2");
-            lineList = lineRepo.getAllLine();
-            writer.write(gson.toJson((lineList)));
+
+        lineList = lineRepo.getAllLines();
+        writer.write(gson.toJson((lineList),List.class));
 
 
         writer.flush();
@@ -39,30 +36,48 @@ public class LineController extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        PrintWriter writer = resp.getWriter();
         Gson gson = new Gson();
         LineRepo lineRepo = new LineRepo();
         Line line;
 
-        line = gson.fromJson(req.getReader(), Line.class);
+
         try {
+            line = gson.fromJson(req.getReader(), Line.class);
             lineRepo.addLines(line);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
+            resp.setStatus(HttpServletResponse.SC_CREATED);
+            writer.write("Line added successfully");
+
+        } catch (JsonSyntaxException | SQLException | ClassNotFoundException e) {
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            writer.write("Error occurred while adding line: " + e.getMessage());
         }
     }
 
-
     @Override
-    protected void doDelete(HttpServletRequest req,HttpServletResponse resp)throws ServletException,IOException{
+    protected void doPut(HttpServletRequest req,HttpServletResponse resp)throws ServletException,IOException{
+        PrintWriter writer = resp.getWriter();
         Gson gson = new Gson();
         LineRepo lineRepo = new LineRepo();
         Line line;
 
         line = gson.fromJson(req.getReader(), Line.class);
-        lineRepo.deleteLine(line);
-
+        lineRepo.updateLine(line);
+        writer.write("Line updated successfully");
+        writer.flush();
+        writer.close();
 
     }
+
+//    @Override
+//    protected void doDelete(HttpServletRequest req,HttpServletResponse resp)throws ServletException,IOException{
+//        Gson gson = new Gson();
+//        LineRepo lineRepo = new LineRepo();
+//        Line line;
+//
+//        line = gson.fromJson(req.getReader(), Line.class);
+//        lineRepo.deleteLine(line);
+//
+//
+//    }
 }
