f5491b4c5d96a68e58a903d27688c39e33acd12a
pehan <pehamrathnayake159@gmail.com>
2024-04-20 16:56:54 +0530

    implement sm/parcel select feature

--- src/main/java/org/ucsc/railboostbackend/controllers/ParcelTrackingController.java
+++ src/main/java/org/ucsc/railboostbackend/controllers/ParcelTrackingController.java
@@ -1,2 +1,38 @@
-package org.ucsc.railboostbackend.controllers;public class ParcelTrackingController {
+package org.ucsc.railboostbackend.controllers;
+
+import com.google.gson.Gson;
+import io.jsonwebtoken.Claims;
+import org.ucsc.railboostbackend.models.ParcelTracking;
+import org.ucsc.railboostbackend.models.Staff;
+import org.ucsc.railboostbackend.repositories.ParcelTrackingRepo;
+import org.ucsc.railboostbackend.repositories.StaffRepo;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+public class ParcelTrackingController extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        PrintWriter writer =resp.getWriter();
+        Gson gson = new Gson();
+        ParcelTrackingRepo parcelTrackingRepo = new ParcelTrackingRepo();
+        StaffRepo staffRepo = new StaffRepo();
+        List<ParcelTracking> parcelTrackingList;
+
+
+        Claims jwt = (Claims) req.getAttribute("jwt");
+        try {
+            int userId = jwt.get("userId", Integer.class);
+            Staff staff = staffRepo.getStaffByUserId(userId);
+            String station = staff.getStation();
+            parcelTrackingList = parcelTrackingRepo.getTrackingParcelsByID(station);
+            writer.write(gson.toJson(parcelTrackingList));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
