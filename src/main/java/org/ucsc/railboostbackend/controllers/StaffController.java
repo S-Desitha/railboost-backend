139055aef51ed53d154147c786ec3fc91249cdbf
Kalindu Sankalpa <kalindusankalpa5285@gmail.com>
2024-04-28 23:34:34 +0530

    Staff id to dashboard / parcel recive after arrival confirm

--- src/main/java/org/ucsc/railboostbackend/controllers/StaffController.java
+++ src/main/java/org/ucsc/railboostbackend/controllers/StaffController.java
@@ -2,8 +2,11 @@ package org.ucsc.railboostbackend.controllers;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import io.jsonwebtoken.Claims;
+import org.ucsc.railboostbackend.models.Season;
 import org.ucsc.railboostbackend.models.Staff;
 import org.ucsc.railboostbackend.models.StaffSignup;
+import org.ucsc.railboostbackend.repositories.SeasonRepo;
 import org.ucsc.railboostbackend.repositories.StaffRepo;
 import org.ucsc.railboostbackend.services.*;
 
@@ -21,28 +24,56 @@ public class StaffController extends HttpServlet {
     
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PrintWriter writer = resp.getWriter();
-        Gson gson = new GsonBuilder()
-                .registerTypeAdapter(LocalDate.class, new LocalDateSerializer())
-                .create();
-        StaffRepo staffRepo = new StaffRepo();
-        List<Staff> staffList;
-        Staff staff;
+        String viewParam = req.getParameter("view");
+        if (viewParam != null && viewParam.equals("1")) {
+            PrintWriter writer = resp.getWriter();
+            Claims jwt = (Claims) req.getAttribute("jwt");
+            Object id = jwt.get("userId");
+            String sid = (id != null) ? id.toString() : "";
+            Gson gson = new GsonBuilder()
+                    .registerTypeAdapter(LocalDate.class, new LocalDateDeserializer())
+                    .registerTypeAdapter(LocalDate.class, new LocalDateSerializer())
+                    .setDateFormat("MM/dd/yyyy")
+                    .create();
+            StaffRepo staffRepo = new StaffRepo();
+            Staff staff = new Staff();
+
+
+            if (sid != null){
+                staff = staffRepo.getStationOfSM(sid);
+                if (staff != null){
+                    writer.write(gson.toJson(staff));
+                } else {
+                    resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+                }
+            } else {
+                resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            }
+            writer.flush();
+            writer.close();
+        } else{
+            PrintWriter writer = resp.getWriter();
+            Gson gson = new GsonBuilder()
+                    .registerTypeAdapter(LocalDate.class, new LocalDateSerializer())
+                    .create();
+            StaffRepo staffRepo = new StaffRepo();
+            List<Staff> staffList;
+            Staff staff;
 //        System.out.println(req.getHeader("origin"));
 
-        String staffId = req.getParameter("staffId");
+            String staffId = req.getParameter("staffId");
 
-        if (staffId!=null){
-            staff = staffRepo.getStaffById(staffId);
-            writer.write(gson.toJson(staff));
-        }
-        else {
-            staffList = staffRepo.getAllStaff();
-            writer.write(gson.toJson(staffList));
-        }
+            if (staffId != null) {
+                staff = staffRepo.getStaffById(staffId);
+                writer.write(gson.toJson(staff));
+            } else {
+                staffList = staffRepo.getAllStaff();
+                writer.write(gson.toJson(staffList));
+            }
 
-        writer.flush();
-        writer.close();
+            writer.flush();
+            writer.close();
+        }
     }
 
 
